cb0f014569c1ca10b2796ee0da2267e78e7322ee
unknown <rachitshukla08@gmail.com>
2020-11-19 19:05:01 +0530

    Added: getContactDataFromStorage function to retrieve and populate data from local storage in tabular form

--- js/HomePage.js
+++ js/HomePage.js
@@ -1,29 +1,32 @@
+let addressBookList;
 window.addEventListener('DOMContentLoaded',(event)=>{
-  createInnerHtml();
+    addressBookList = getContactDataFromStorage();
+    document.querySelector(".add-count").textContent = addressBookList.length;
+    createInnerHtml();
 });
 
 const createInnerHtml = () => {
     const headerHtml = "<th></th><th>Fullname</th><th>Address</th><th>City</th>"
                         +"<th>State</th><th>Zip Code</th><th>Phone Number</th>";
     let innerHtml = `${headerHtml}`;
-    let addressBookList = createContactsJSON();
     for(const contact of addressBookList){
+      console.log(contact);
       innerHtml = `${innerHtml}
-    <tr><td></td>
-    <td>${contact._name}</td>
-    <td>${contact._address}</td>
-    <td>${contact._city}</td>
-    <td>${contact._state}</td>
-    <td>${contact._zip}</td>
-    <td>${contact._phone}</td>
-    <td>
-        <img id="1" onclick="remove(this)" alt="delete"
-            src="../assets/icons/delete-black-18dp.svg">
-        <img id="1" alt="edit" onclick="update(this)" 
-            src="../assets/icons/create-black-18dp.svg">
-    </td>
-    </tr>
-    `;
+      <tr><td></td>
+      <td>${contact._name}</td>
+      <td>${contact._address}</td>
+      <td>${contact._city}</td>
+      <td>${contact._state}</td>
+      <td>${contact._zip}</td>
+      <td>${contact._phone}</td>
+      <td>
+          <img id="1" onclick="remove(this)" alt="delete"
+              src="../assets/icons/delete-black-18dp.svg">
+          <img id="1" alt="edit" onclick="update(this)" 
+              src="../assets/icons/create-black-18dp.svg">
+      </td>
+      </tr>
+      `;
     }
     document.querySelector('#table-display').innerHTML = innerHtml;
 }
@@ -47,4 +50,9 @@ const createContactsJSON = () => {
     },
   ];
   return addressBookListLocal;
+}
+
+const getContactDataFromStorage = () => {
+  return localStorage.getItem('AddressBookList')?
+                              JSON.parse(localStorage.getItem('AddressBookList')):[];
 }
